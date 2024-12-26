14ce4a4c21354aa7e23a6377c88313cb7e249441
JanaThuluth <s12325336@stu.najah.edu>
2024-12-26 19:16:48 +0200

    update/card

--- src/cmponent/card/Card.js
+++ src/cmponent/card/Card.js
@@ -4,10 +4,10 @@ const Card = () =>{
     return(
         <div id ="card"> 
         <button>bookNow</button>
-        
+        <h1> hello team project 8</h1>
         </div>
     );
      };
     
      export default Card;
-     
+
