2d3dda5c5b13efcd1699e9fa5392e4fe25c1b478
JoodAbdelhaq <s12323183@stu.najah.edu>
2025-01-03 22:12:27 +0200

    card,counter,hero

---
+++ src/Counter.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./Counter.css"; 
+
+const Counter =()  =>{
+  const [count, setCount] = useState(1); 
+  return (
+    <div className="counter-container">
+      <button
+        className="counter-button"
+        onClick={() => setCount((prevCount) => Math.max(prevCount - 1, 1))}
+      >
+        -
+      </button>
+      <span className="counter-value">{count}</span>
+      <button
+        className="counter-button"
+        onClick={() => setCount((prevCount) => prevCount + 1)}
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
