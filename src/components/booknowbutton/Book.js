d710364768c9632519b494fe250b7b5230b9d991
Raghad-M-Banyodi <s12325002@stu.najah.edu>
2025-01-03 22:55:08 +0200

    raghadupdate

---
+++ src/components/booknowbutton/Book.js
@@ -0,0 +1,12 @@
+import './stylebutton.css';
+
+const Book = () => {
+    return(
+
+  <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
+  Book Now
+  </button>
+
+);
+};
+export default Book;
