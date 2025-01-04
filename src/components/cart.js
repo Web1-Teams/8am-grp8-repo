a420788bfda1f1828efa14745d62c1e45a42b060
waseem-2004 <waseemsalahat11@gmail.com>
2025-01-04 16:47:38 +0200

    Calls

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,5 +1,49 @@
+import React from "react";
+import Counter from "./Counter"; 
+import "../styles/cart.css";
 
-import '../styles/cart.css';
+const Cart = ({ cart, setCart }) => {
+  const handleAmountChange = (id, newAmount) => {
+    const updatedCart = cart.map((item) =>
+      item.id === id ? { ...item, amount: newAmount } : item
+    );
+    setCart(updatedCart);
+  };
+
+  return (
+    <article>
+      {cart?.map((item) => (
+        <div className="cart_box" key={item.id}>
+          <div className="cart_img">
+            <img src={item.img} alt="product" />
+            <p>{item.title}</p>
+          </div>
+          <div>
+            <Counter
+              count={item.amount || 1}
+              onCountChange={(newCount) => handleAmountChange(item.id, newCount)}
+            />
+          </div>
+          <div>
+            <span>${item.price}</span>
+            <button>Remove</button>
+          </div>
+        </div>
+      ))}
+      <div className="total">
+        <span>Total price of your cart</span>
+        <span>
+          Rs-
+          {cart.reduce((total, item) => total + item.price * (item.amount || 1), 0)}
+        </span>
+      </div>
+    </article>
+  );
+};
+
+export default Cart;
+
+/*import '../styles/cart.css';
 
 const Cart=({cart})=>{
     
@@ -32,4 +76,4 @@ const Cart=({cart})=>{
 }
 
 
-export default Cart;
+export default Cart;*/
