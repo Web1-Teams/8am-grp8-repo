c44985db5419fd97a240c79f9ffe4de6a9ed581f
JanaThuluth <s12325336@stu.najah.edu>
2025-01-04 02:21:41 +0200

    gi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
+
 import Navbar from './components/Navbar'
 
 import SpecialTitle from './components/SpecialTitle'
+import OrderForm from './components/Form/OrderForm'
 
 const App = () => {
   return (
   <div className="mt-40">
-   <SpecialTitle title ="janaaaaaaaaaaaa" className="text-center"/>
-   <Navbar/>
-   
+   <SpecialTitle title ="jana" className="text-center"/>
+  <Navbar/>
+   <OrderForm/>
   </div>
   )
 }
