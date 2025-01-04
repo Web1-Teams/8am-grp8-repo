98242d223b516154517bbd9633f66bf1692219fc
zeyad <s12218555@stu.najah.edu>
2025-01-04 15:39:01 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 
 
-import Navbar from './components/Navbar'
 
-import SpecialTitle from './components/SpecialTitle'
 import OrderForm from './components/Form/OrderForm'
 
 const App = () => {
   return (
   <div className="mt-40">
-   <SpecialTitle title ="jana" className="text-center"/>
-  <Navbar/>
+   
    <OrderForm/>
   </div>
   )
