8dd457c67f14eaae19615f8b2fbe50a40b65085e
JanaThuluth <s12325336@stu.najah.edu>
2025-01-04 15:19:33 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
 
 import Navbar from './components/Navbar'
-
+import Footer from './components/Footer/Footer'
 import SpecialTitle from './components/SpecialTitle'
-import OrderForm from './components/Form/OrderForm'
 
 const App = () => {
   return (
   <div className="mt-40">
    <SpecialTitle title ="jana" className="text-center"/>
   <Navbar/>
-   <OrderForm/>
+   
+   <Footer/>
   </div>
   )
 }
