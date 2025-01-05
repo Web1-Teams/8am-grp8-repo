ab3c4d49ffe9d4465fff009a93f3b99b8428f81b
waseem-2004 <waseemsalahat11@gmail.com>
2025-01-05 15:47:06 +0200

    newUpdate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Home from "./components/Pages/Home";
--- src/components/Pages/Cakes.js
+++ src/components/Pages/Cakes.js
@@ -1,8 +1,16 @@
+import Banner from "../Banner/Banner";
+import SpecialTitle from "../SpecialTitle";
+import cakeB from '../assest/images/imageW/cake.jpg';
 
 const Cakes = () => {
   
   return (
-    <div className="mt-40">login</div>
+    <div className="mt-40">
+      <Banner image={cakeB}/>
+      <SpecialTitle title="Cake & Gâteau"/>
+      
+    </div>
+    
   );
 };
 
