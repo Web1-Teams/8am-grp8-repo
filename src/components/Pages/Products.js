0b1b14562e6c98bfa10cca965737fa61f640a58a
JanaThuluth <s12325336@stu.najah.edu>
2025-01-06 13:41:34 +0200

    final

--- src/components/Pages/Products.js
+++ src/components/Pages/Products.js
@@ -2,7 +2,7 @@ import SpecialTitle from "../specialtitle/SpecialTitle";
 
 const Products = () => {
   return (
-    <div className="mt-40">
+    <div className="mt-4">
       <SpecialTitle title={"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"} className={"text-center"} />
     </div>
   );
